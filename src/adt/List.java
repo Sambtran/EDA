2301cc8c1a9dca0424c6553dc663f9cc07ef8eab
Alberto <albertolucascb@gmail.com>
2025-02-25 23:58:23 +0100

    Principio

---
+++ src/adt/List.java
@@ -0,0 +1,14 @@
+package adt;
+import exception.WrongIndexException;
+import java.util.Iterator;
+
+
+public interface List<E> extends Iterable<E> {
+    void insert(int pos, E data) throws WrongIndexException;
+    void delete(int pos) throws WrongIndexException;
+    E get(int pos) throws WrongIndexException;
+    int search(E data);
+
+    Iterator<E> iterator();
+    int size();
+}
---
+++ src/solutions/testlista.java
@@ -0,0 +1,16 @@
+package solutions;
+import ds.lista;
+import exception.WrongIndexException;
+
+public class testlista {
+    public static void main(String[] args) throws WrongIndexException {
+        lista ls = new lista();
+        ls.insert(0,2);
+        ls.insert(1,3);
+        ls.insert(2,4);
+        ls.delete(1);
+        System.out.println(ls.search(3));
+
+    }
+
+}
